c55c00976fe35ed1956e48013b2b2347ad94cf26
Angus Salkeld <asalkeld@mirantis.com>
2014-08-22 14:16:51 +1000

    Fix cloud-init-run script
    Error when copying it from the supervisor config.

--- cloud-init/cloud-init-run.sh
+++ cloud-init/cloud-init-run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # supervisor doesn't like processes that exit, so fake it.
-command=/usr/bin/cloud-init -d init && tailf /var/log/cloud-init-output.log
+cloud-init -d init && tailf /var/log/cloud-init-output.log
